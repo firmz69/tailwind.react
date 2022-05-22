26502635bf3162984c8cb3f7267da5b2f7e69584
firmz69 <jognejong@gmail.com>
2022-05-22 13:16:18 +0700

    Menambahkan file tailwind-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Product from './components/Product'
 import Contact from './components/Contact'
-import React from 'react'
 
 function App() {
   return (
