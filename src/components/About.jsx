f3ea2205911437a2680a2b02059a384f42d9c562
firmz69 <jognejong@gmail.com>
2022-05-22 14:00:51 +0700

    Menambahkan file tailwind-react

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,7 @@ export default function Main() {
        <h2 className="mb-4 text-2xl font-bold sm:text-4xl text-cyan-900">
           Syanaz Musik</h2>
          <p className="mb-12 px-0 text-lg text-zinc-500 lg:px-52 ">
-          Toko kami merupakan toko alat musik professional dan pro audio online terlengkap di Indonesia. Alat musik yang kami sediakan diantaranya Gitar Akustik, Gitar Elektrik, Drum Set, Ampli Gitar, Efek Gitar, Ampli Gitar Bass, Power Amplifier, Microphone dll.</p>
-         <a href="/" className="underline decoration-solid font-serif font-medium">
-          Tentang Kami</a>       
+          Toko kami merupakan toko alat musik professional dan pro audio online terlengkap di Indonesia. Alat musik yang kami sediakan diantaranya Gitar Akustik, Gitar Elektrik, Drum Set, Ampli Gitar, Efek Gitar, Ampli Gitar Bass, Power Amplifier, Microphone dll.</p>      
       </div>
     );
   };
